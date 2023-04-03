d4acd9225310fb6fd7ab0af96e832622f1475d55
Dohyun Moon <d6moon@uwaterloo.ca>
2023-04-02 23:53:12 -0400

    fixed movement logic and added picture of knight

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
     t.window.width = 800
-    t.window.height = 600
+    t.window.height = 800
     t.window.title = "My Love Game"
     t.window.resizable = false
 end
--- main.lua
+++ main.lua
@@ -1,11 +1,46 @@
 X, Y = 0, 0
 
-function love.draw()
-    love.graphics.print(tostring(X), 300, 300)
-    love.graphics.print(tostring(Y), 500, 300)
+PREV_PRESSED = ''
+CURRENT_COMBINATION = "nomove"
+UPDATE = false
+
+function love.load()
+    pos_x = 4
+    pos_y = 4
+
+    white = {1, 1, 1}
+    black = {0, 0, 0}
+
+    image = love.graphics.newImage("assets/knight.png")
 end
 
-local directions = {
+function love.update(dt)
+    if UPDATE then
+        local x_val = pos_x + X
+        local y_val = pos_y + Y
+        if x_val > 0 and x_val < 7 and y_val > 0 and y_val < 7 then
+            pos_x = x_val
+            pos_y = y_val
+        end
+    end
+    UPDATE = false
+end
+
+function love.draw()
+    for i = 0, 7 do
+        for j = 0, 7 do
+            if (i + j) % 2 == 0 then
+                love.graphics.setColor(white)
+            else
+                love.graphics.setColor(black)
+            end
+            love.graphics.rectangle("fill", i * 100, j * 100, 100, 100)
+        end
+    end
+    love.graphics.draw(image, pos_x*100, pos_y*100)
+end
+
+DIRECTIONS = {
     ["upleft"] = { x = -1, y = -2 },
     ["upright"] = { x = 1, y = -2 },
     ["leftup"] = { x = -2, y = -1 },
@@ -18,37 +53,32 @@ local directions = {
 }
 
 function love.keypressed(key)
-    local current_combination = "nomove"
-    
-    -- check which keys are currently pressed
-    if love.keyboard.isDown("w") then
-        if love.keyboard.isDown("a") then
-            current_combination = "upleft"
-        elseif love.keyboard.isDown("d") then
-            current_combination = "upright"
+
+    if PREV_PRESSED == '' then
+        PREV_PRESSED = key
+    else
+        if PREV_PRESSED == "up" and love.keyboard.isDown("left") then
+            CURRENT_COMBINATION = "upleft"
+        elseif PREV_PRESSED == "up" and love.keyboard.isDown("right") then
+            CURRENT_COMBINATION = "upright"
+        elseif PREV_PRESSED == "left" and love.keyboard.isDown("up") then
+            CURRENT_COMBINATION = "leftup"
+        elseif PREV_PRESSED == "left" and love.keyboard.isDown("down") then
+            CURRENT_COMBINATION = "leftdown"
+        elseif PREV_PRESSED == "down" and love.keyboard.isDown("right") then
+            CURRENT_COMBINATION = "downright"
+        elseif PREV_PRESSED == "down" and love.keyboard.isDown("left") then
+            CURRENT_COMBINATION = "downleft"
+        elseif PREV_PRESSED == "right" and love.keyboard.isDown("up") then
+            CURRENT_COMBINATION = "rightup"
+        elseif PREV_PRESSED == "right" and love.keyboard.isDown("down") then
+            CURRENT_COMBINATION = "rightdown"
+        else
+            CURRENT_COMBINATION = "nomove"
         end
-    end
-    if love.keyboard.isDown("a") then
-        if love.keyboard.isDown("w") then
-            current_combination = "leftup"
-        elseif love.keyboard.isDown("s") then
-            current_combination = "leftdown"
-        end
-    end
-    if love.keyboard.isDown("s") then
-        if love.keyboard.isDown("a") then
-            current_combination = "downleft"
-        elseif love.keyboard.isDown("d") then
-            current_combination = "downright"
-        end
-    end
-    if love.keyboard.isDown("d") then
-        if love.keyboard.isDown("w") then
-            current_combination = "rightup"
-        elseif love.keyboard.isDown("s") then
-            current_combination = "rightdown"
-        end
+        PREV_PRESSED = ''
+        UPDATE = true
     end
 
-    X, Y = directions[current_combination].x, directions[current_combination].y
+    X, Y = DIRECTIONS[CURRENT_COMBINATION].x, DIRECTIONS[CURRENT_COMBINATION].y
 end
